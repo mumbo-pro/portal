e087052333e355938c6f011b1c6cbd68e963ef54
Alpha Olomi <alphaolomi@gmail.com>
2019-10-23 01:44:48 +0300

    improvement(app): update
    - update procfile  to latest server file

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const config = require("config");
+var express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('config');
 
-var logger = require("morgan");
+var logger = require('morgan');
 
 var app = express();
 
-const db = config.get("MONGO_URI");
+const db = config.get('MONGO_URI');
 
 mongoose
   .connect(db, {
@@ -15,16 +15,15 @@ mongoose
     useCreateIndex: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log("MongoDB Error:" + err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('MongoDB Error:' + err));
 
-app.use(logger("dev"));
-app.use(express.json());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/jobs", require("./routes/api/job"));
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
+app.use('/api/jobs', require('./routes/api/job'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 module.exports = app;
